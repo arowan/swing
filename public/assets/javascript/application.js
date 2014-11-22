2fd15eee798e176eeb9958f9f63a774745b2b334
Alex Rowan <arowanuk@gmail.com>
2014-11-22 18:05:10 +0000

    ooo star

--- public/assets/javascript/application.js
+++ public/assets/javascript/application.js
@@ -11,6 +11,7 @@ function preload() {
 
 }
 
+
 var player;
 var platforms;
 var cursors;
@@ -70,18 +71,16 @@ function create() {
     //  We will enable physics for any star that is created in this group
     stars.enableBody = true;
 
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
+    _(98).times(function(i){
+        var star = stars.create(70, 0, 'star');
 
         //  Let gravity do its thing
         star.body.gravity.y = 300;
 
         //  This just gives each star a slightly random bounce value
         star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
+    })
+
 
     //  The score
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
